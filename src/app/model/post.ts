719c5e9b66ca3c45e4e482334e19464d394749fb
Philipp Brendel <waldrumpus@gmail.com>
2021-07-31 11:48:37 +0200

    add  creation and change  date to post

--- src/app/model/post.ts
+++ src/app/model/post.ts
@@ -2,10 +2,14 @@
 export interface PostHeader {
     id: string,
     title: string,
+    created: Date,
+    changed: Date,
 }
 
 export interface Post {
     id: string,
     title: string,
     text: string,
+    created: Date,
+    changed: Date,
 }
